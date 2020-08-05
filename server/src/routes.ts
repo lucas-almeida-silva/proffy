0d6a9b2962d9684a44cba54fd1e7a006d441347d
Lucas Almeida da Silva <lucas.silva@mttechne.com.br>
2020-08-04 22:49:37 -0300

    Criação da API REST em Node.js

---
+++ server/src/routes.ts
@@ -0,0 +1,15 @@
+import express, { text } from 'express';
+import ClassesController from './controllers/ClassesController';
+import ConnectionsController from './controllers/ConnectionsController';
+
+const routes = express.Router();
+const classesConstroller = new ClassesController();
+const connectionsController = new ConnectionsController();
+
+routes.get('/classes', classesConstroller.index);
+routes.post('/classes', classesConstroller.create);
+
+routes.get('/connections', connectionsController.index);
+routes.post('/connections', connectionsController.create);
+
+export default routes;
