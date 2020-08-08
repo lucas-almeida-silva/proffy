0453ce769430fc2a7a3a17da792651e8b5bd58a7
Lucas Almeida da Silva <lucas_almeidasilva01@hotmail.com>
2020-08-08 00:37:37 -0300

    Filtro de professores e adição/remoção de professores nos favoritoas na versão mobile

--- mobile/src/pages/Favorites/styles.tsx
+++ mobile/src/pages/Favorites/styles.tsx
@@ -6,6 +6,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#f0f0f7'
   },
 
+  teacherList: {
+    marginTop: -40,
+  }
 
 });
 
--- mobile/src/pages/TeacherList/styles.tsx
+++ mobile/src/pages/TeacherList/styles.tsx
@@ -6,6 +6,52 @@ const styles = StyleSheet.create({
     backgroundColor: '#f0f0f7'
   },
 
+  teacherList: {
+    marginTop: -40,
+  },
+
+  searchForm: {
+    marginBottom: 24
+  },
+
+  label: {
+    color: '#d4c2ff',
+    fontFamily: 'Poppins_400Regular'
+  },
+
+  InputGroup: {
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+
+  input: {
+    height: 54,
+    backgroundColor: '#FFF',
+    borderRadius: 8,
+    justifyContent: 'center',
+    paddingHorizontal: 16,
+    marginTop: 4,
+    marginBottom: 16
+  },
+
+  inputBlock: {
+    width: '48%'
+  },
+
+  submitButton: {
+    backgroundColor: '#04d361',
+    height: 56,
+    borderRadius: 8,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+
+  submitButtonText: {
+    color: '#FFF',
+    fontFamily: 'Archivo_700Bold',
+    fontSize: 16,
+  }
 
 });
 
